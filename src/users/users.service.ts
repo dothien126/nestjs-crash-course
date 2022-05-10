2ea858a31e29582f13eef2b115e57b9f25bea7ff
dothien126 <dothien2601ak39@gmail.com>
2022-05-10 09:31:36 +0700

    CRUD, relationsssss

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import {User} from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-    private users: User[] = [{id: 1, name: 'imthien'}]
+    constructor(
+        @InjectRepository(User) private usersRepository: Repository<User>
+    ) {}
 
-    findAll(name?: string): User[] {
-        if(name) {
-            return this.users.filter(user => user.name ===name)
-        }
-        return this.users;
+    async getAll(): Promise<User[]> {
+        return await this.usersRepository.find();  // SELECT * FROM user
     }
 
-    findById(userId: number): User {
-        return this.users.find(user => user.id === userId)
+    async getUserById(id: number): Promise<User> {
+        try {
+            const user = await this.usersRepository.findOneOrFail({where: {id}}); // SELECT * FROM user WHERE user.id = id 
+            return user;
+        } catch (err) {
+            // handle error
+            throw err;
+        } 
     }
 
-    createUser(createUserDto: CreateUserDto): User {
-        const newUser = {id: Date.now(), ...createUserDto};
-
-        this.users.push(newUser)
-
-        return newUser
+    async createUser(name: string): Promise<User> {
+        const newUser = this.usersRepository.create({name}); // const user = new User()
+        return await this.usersRepository.save(newUser); // insert new  user
     }
+ 
+    async updateUser(id: number, name: string): Promise<User> {
+        const user = await this.getUserById(id)
+        user.name = name
+        return this.usersRepository.save(user) // update
+    }
+
+    async deleteUser(id: number): Promise<User> {
+        const user = await this.getUserById(id)
+        return this.usersRepository.remove(user)
+    }
+
+    customQuery(): any {
+        return this.usersRepository.createQueryBuilder('user').select('name');
+    }
+
 }
